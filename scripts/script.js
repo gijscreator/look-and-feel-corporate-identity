39153c8dbaf4df0853b1067e0f30a22ac0b64fd8
gijscreator <65440619+gijscreator@users.noreply.github.com>
2025-11-26 09:35:02 +0100

    added timer

--- scripts/script.js
+++ scripts/script.js
@@ -42,7 +42,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const locationArray = document.querySelector(".location-array");
 
   const onDetailPage = bigImage && nameText && placeText;
-
   // ============================================
   //  DETAIL PAGE LOGIC
   // ============================================
@@ -161,4 +160,40 @@ if (likee) {
   likee.addEventListener("click", () => {
     likee.classList.toggle("open");
   });
+}
+
+const clock = document.getElementById("countdown");
+
+window.onload = () => {
+    if (document.getElementById("countdown")) {
+        timer();
+    }
+}
+
+function timer() {
+    console.log("coutdown started")
+    let seconds = Math.floor(Math.random() * (21600 - 3600) + 3600);
+    setInterval(() => {
+        seconds--;
+        clock.textContent = timeFormat(seconds);
+    }, 1000);
+
+    if (seconds <= 0) {
+        clock.textContent = "je was op niks aan het wachten"
+    }
+}
+
+
+function timeFormat(seconds) {
+    // uitrekenen hoeveel uren minuten en seconden er moeten zijn
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60)
+    const secs = seconds % 60;
+
+    // zorgen dat er een 0 voorkomt zodat 1:3:2 -> 01:03:02 word
+    const h = String(hours).padStart(2, "0");
+    const m = String(minutes).padStart(2, "0");
+    const s = String(secs).padStart(2, "0");
+
+    return `${h}:${m}:${s}`
 }
